69516a0c86d22671473feaa3cd10b5bab53842bb
makotos <makotos.nlproc@gmail.com>
2024-02-12 08:46:05 +0900

    fix run.s2t.fixed_predecision.asr_pretrain.sh

--- shell_scripts/run.s2t.fixed_predecision.asr_pretrain.sh
+++ shell_scripts/run.s2t.fixed_predecision.asr_pretrain.sh
@@ -1,17 +1,23 @@
+export PYTHONPATH="~/ITST-reproduction/examples:$PYTHONPATH"
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-mustc_root=PATH_TO_MUSTC_DATA
+EXP_NAME=$1
+
+mustc_root=~/ITST-Reproduction/data
 lang=de
-asr_modelfile=PATH_TO_SAVE_ASR_MODEL
+# mkdir checkopoints
+asr_modelfile=~/ITST-Reproduction/checkpoints/${EXP_NAME}
+mkdir -p ${asr_modelfile}
 
 python train.py ${mustc_root}/en-${lang} \
   --config-yaml config_asr.yaml --train-subset train_asr --valid-subset dev_asr \
   --save-dir ${asr_modelfile} --num-workers 4 --max-update 100000 --max-tokens 30000  \
   --task speech_to_text --criterion label_smoothed_cross_entropy --report-accuracy \
-  --arch convtransformer_espnet --optimizer adam --lr 0.0005 --lr-scheduler inverse_sqrt \
+  --arch convtransformer_espnet --optimizer adam --lr 0.00005 --lr-scheduler inverse_sqrt \
   --warmup-updates 10000 --clip-norm 10.0 --seed 1 --update-freq 3 \
   --save-interval-updates 1000 \
   --keep-interval-updates 100 \
   --find-unused-parameters \
   --fp16 \
-  --log-interval 10
+  --log-interval 10 \
+  --wandb-project itst-asr-pretrain-${EXP_NAME}
