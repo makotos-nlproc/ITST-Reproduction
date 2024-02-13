c4e19474e3b7973204aca521062e82b8ca16db4f
makotos <makotos.nlproc@gmail.com>
2024-02-13 17:27:23 +0900

    fix sh for itst fixed predesicion

--- shell_scripts/run.s2t.itst.fixed_predecision.sh
+++ shell_scripts/run.s2t.itst.fixed_predecision.sh
@@ -1,9 +1,16 @@
+export PYTHONPATH="~/ITST-reproduction/examples:$PYTHONPATH"
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-mustc_root=PATH_TO_MUSTC_DATA
+# TODO: check arg for exp_name
+EXP_NAME=$1
+
+mustc_root=~/ITST-Reproduction/data
 lang=de
-asr_modelfile=PATH_TO_SAVE_ASR_MODEL
-st_modelfile=PATH_TO_SAVE_ST_MODEL
+asr_modelfile=~/ITST-Reproduction/asr_model/${EXP_NAME}
+# mkdir st_model
+st_modelfile=~/ITST-Reproduction/st_model/${EXP_NAME}
+mkdir -p ${st_modelfile}
+pretrain_model=checkpoint_best.pt
 
 python train.py ${mustc_root}/en-${lang} \
     --config-yaml config_st.yaml --train-subset train_st --valid-subset dev_st \
@@ -13,7 +20,7 @@ python train.py ${mustc_root}/en-${lang} \
     --criterion label_smoothed_cross_entropy_with_itst_s2t_fixed_predecision \
     --warmup-updates 4000 --max-update 300000 --max-tokens 20000 --seed 2 \
     --label-smoothing 0.1 \
-    --load-pretrained-encoder-from ${asr_modelfile}/best.pt \
+    --load-pretrained-encoder-from ${asr_modelfile}/${pretrain_model} \
     --task speech_to_text  \
     --arch convtransformer_simul_trans_itst_espnet  \
     --unidirectional-encoder \
@@ -24,4 +31,5 @@ python train.py ${mustc_root}/en-${lang} \
     --keep-interval-updates 200 \
     --find-unused-parameters \
     --fp16 \
-    --log-interval 10 
+    --log-interval 10 \
+    --wandb-project itst-st-train-${EXP_NAME}
